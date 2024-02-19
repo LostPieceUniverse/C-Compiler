d2eb0f2f377c5c8ebcc461a7826d988b89a9d5b5
fedora <sam.zg@outlook.com>
2024-02-19 12:01:56 +0100

    noon

--- cCompiler/Parser/ExpressionTree.cs
+++ cCompiler/Parser/ExpressionTree.cs
@@ -12,14 +12,57 @@ namespace Compiler
     }
     static public ExpressionTree Build(List<Token> tokenList)
     {
-      
-      return null;
+      //berry unpretty solution incomming...I think...
+      ExpressionTree rootNode = null;
+      foreach (Token token in tokenList)
+      {
+        if(token.Type == TokenType.IntegerLiteral)
+        {
+          rootNode = IntegerLiteralExpressionNode.BuildAST(tokenList);
+        }
+        else if(token.Type == TokenType.StringLiteral)
+        {
+
+        }
+      }
+      return rootNode;
     }
   }
 
   public class IntegerLiteralExpressionNode : ExpressionTree
   {
+    static public IntegerLiteralExpressionNode BuildAST(List<Token> tokenList)
+    {
+      Token token = tokenList[0];
+      tokenList.RemoveAt(0);
 
+      IntegerLiteralExpressionNode node = new IntegerLiteralExpressionNode();
+
+      if(token.TokenType == TokenType.OpenParenthesis)
+      {
+        node = BuildAST(tokenList);
+        //remove the closing parenthesis
+        tokenList.RemoveAt(0);
+        return expression;
+      }
+      else if(IsOperator)
+      {
+        node.Operator = this.Operator;
+        node.Left = BuildAST(tokenList);
+        node.Right = BuildAST(tokenList);
+        return node;
+      }
+
+    }
+    
+    public IntegerLiteralExpressionNode Left { get; set;} = null;
+    public IntegerLiteralExpressionNode Right { get; set;} = null;
+
+    //public OperatorType Operator { get; set; } = OperatorType.muffin;
+    public TokenType Operator { get; set; } = OperatorType.muffin;//temp
+
+    public string Operand { get; set; } = string.Empty;
+    public bool IsOperator { get; set; } = false;
   }
 
   public class StringLiteralExpressionNode : ExpressionTree
